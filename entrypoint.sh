91eb25b293bb11cc60316448e04d6ee6a96f1a24
jr0w3 <jonathan.languin@gmail.com>
2023-11-15 17:28:18 +0100

    Add VERBOSE ENV VAR

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,9 @@ cd /app/glpiCT
 # Activation de l'environement Python
 source .venv/bin/activate
 
+if [ "$VERBOSE" = true ];then
+    VERBOSEOPT="--verbose"
+fi
+
 # Éxécution du script avec les variable d'environement
-python3 main.py -u $GLPI_URL -t $API_TOKEN -s $SEUIL_ALERT -p $SERVEUR_PORT -m $SERVEUR_SMTP -d $EMAIL_DEST --sav $ADMIN_EMAIL --savfrom $ADMIN_FROM_EMAIL
+python3 main.py -u $GLPI_URL -t $API_TOKEN -s $SEUIL_ALERT -p $SERVEUR_PORT -m $SERVEUR_SMTP -d $EMAIL_DEST --sav $ADMIN_EMAIL --savfrom $ADMIN_FROM_EMAIL $VERBOSEOPT
